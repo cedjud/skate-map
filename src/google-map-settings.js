91f1d7e629c6cd00013f2420b63ffa3c71d58e7d
ribsopen <info@ribsopen.com>
2018-01-18 23:58:36 +0100

    Update google maps api key

--- src/google-map-settings.js
+++ src/google-map-settings.js
@@ -1,4 +1,5 @@
-export const googleMapURL = "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyCahLFd-VW58iG7AxPhRTLNPcRGf9SeFPY";
+// export const googleMapURL = "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyCahLFd-VW58iG7AxPhRTLNPcRGf9SeFPY";
+export const googleMapURL = "https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places&key=AIzaSyCzgpJxOkHxWpwL8KEBYi_WR0XShP22H7k";
 
 export const containerElementStyles = {
   width: '100vw',
